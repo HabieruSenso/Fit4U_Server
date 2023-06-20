e0b3f6b6a29853ee3449ff7a27e56f08ddb9a21c
HabieruSenso <javier.guerra@live.u-tad.com>
2023-06-20 18:27:02 +0200

    otra vez aqui

--- src/repositorio/DatabaseFactory.kt
+++ src/repositorio/DatabaseFactory.kt
@@ -29,18 +29,18 @@ object DatabaseFactory {
     fun hikari(): HikariDataSource {
         val config = HikariConfig()
         config.driverClassName = System.getenv("JDBC_DRIVER") // 1
-        config.jdbcUrl = System.getenv("DATABASE_URL") // 2
+       // config.jdbcUrl = System.getenv("DATABASE_URL") // 2
         config.maximumPoolSize = 3
         config.isAutoCommit = false
         config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
 
-        /*  val uri = URI(System.getenv("DATABASE_URL"))
+          val uri = URI(System.getenv("DATABASE_URL"))
           val username = uri.userInfo.split(":").toTypedArray()[0]
           val password = uri.userInfo.split(":").toTypedArray()[1]
 
           config.jdbcUrl =
               "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require" + "&user=$username&password=$password"
-  */
+
         config.validate()
 
         return HikariDataSource(config)
